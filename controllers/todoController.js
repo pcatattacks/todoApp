a61faee4915ec1f799902521f780cf79a4638128
Pranav Dhingra <pranav.dhingra@gmail.com>
2016-12-24 15:20:56 +0800

    dummy data injected into template using ejs. Next step is to use mongoDB instead of dummy data.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,9 @@
 module.exports = function(app){
 
+var data = [{item:'get milk'},{item: 'walk dog'}, {item: 'code Node'}]; //dummy data
+
 app.get('/todo', function(req, res){
-    res.render('todo.ejs');
+    res.render('todo.ejs', {todos: data});
 });
 
 app.post('/todo', function(req, res){
