cf638f9a9c5c82fb6c74d2e199bf4261347f5130
Pranav Dhingra <pranav.dhingra@gmail.com>
2016-12-25 14:23:35 +0800

    adding and deleting items with ajaz requests to a temporary array is working. Next step is to store data permanently on a NoSQL database, aka. MongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,17 +1,24 @@
+var bodyParser = require('body-parser');
+var urlencodedParser =  bodyParser.urlencoded({extended:false});
+
 module.exports = function(app){
 
 var data = [{item:'get milk'},{item: 'walk dog'}, {item: 'code Node'}]; //dummy data
 
 app.get('/todo', function(req, res){
-    res.render('todo.ejs', {todos: data});
+    res.render('todo', {todos: data});
 });
 
-app.post('/todo', function(req, res){
-
+app.post('/todo', urlencodedParser, function(req, res){
+    data.push(req.body);
+    res.json(data);
 });
 
-app.delete('/todo', function(req, res){
-
+app.delete('/todo/:item', function(req, res){
+    data = data.filter(function(todo){
+        return todo.item.replace(/ /g,'-') !== req.params.item;
+    });
+    res.json(data);
 });
 
 };
---
+++ public/assets/todolist.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('form').on('submit', function(){
+        var item = $('form input');
+        var todo = {item: item.val()};
+
+        $.ajax({
+            type: 'POST',
+            url: '/todo',
+            data: todo,
+            success: function(data){
+                //reload page to reflect changes in list
+                location.reload();
+            }
+        });
+    });
+
+    $('li').on('click', function(){
+        var item = $(this).text().replace(/ /g, "-");
+        
+        $.ajax({
+            type:'DELETE',
+            url: '/todo/'+item,
+            success: function(data){
+                //reload page to reflect changes in list
+                location.reload();
+            }
+        });
+    });
+});
