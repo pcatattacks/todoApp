9f5ecb453eac35837e7a77ce98efd19511f2c4be
Pranav Dhingra <pranav.dhingra@gmail.com>
2016-12-25 15:42:20 +0800

    Todo app completely functional, only CSS stylesheet to be added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,49 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to database
+mongoose.connect('mongodb://test:test@ds145208.mlab.com:45208/pcatsdb');
+
+//create schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+//testing
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+//     if (err) throw err;
+//     console.log('item saved');
+// });
+
 var urlencodedParser =  bodyParser.urlencoded({extended:false});
 
 module.exports = function(app){
 
-var data = [{item:'get milk'},{item: 'walk dog'}, {item: 'code Node'}]; //dummy data
+//var data = [{item:'get milk'},{item: 'walk dog'}, {item: 'code Node'}]; //dummy data
 
 app.get('/todo', function(req, res){
-    res.render('todo', {todos: data});
+    //get data from mongoDB and pass to view
+    Todo.find({}, function(err, data){
+        if (err) throw err;
+        res.render('todo', {todos: data});    
+    });
 });
 
 app.post('/todo', urlencodedParser, function(req, res){
-    data.push(req.body);
-    res.json(data);
+    //get data from view and add it to mongoDB
+    Todo(req.body).save(function(err, data){
+        if (err) throw err;
+        res.json(data);
+    });
 });
 
 app.delete('/todo/:item', function(req, res){
-    data = data.filter(function(todo){
-        return todo.item.replace(/ /g,'-') !== req.params.item;
-    });
-    res.json(data);
+    //delete the requested item from MongoDB
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+        if (err) throw err;
+        res.json(data);
+    }); 
 });
 
 };
